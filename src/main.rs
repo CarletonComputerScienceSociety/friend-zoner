55baf7d91c9550424327445a5ae4bba7c7635aab
Forest Anderson <forestkzanderson@gmail.com>
2021-09-12 15:35:24 -0400

    The main file

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,7 @@
 use std::env;
 
+use serenity::model::channel::{ChannelType, GuildChannel};
+use serenity::model::Permissions;
 use serenity::{
     async_trait,
     model::{
@@ -15,9 +17,15 @@ use serenity::{
     },
     prelude::*,
 };
+use std::str::FromStr;
+use strum_macros::EnumString;
+use tracing::{debug, error, warn, Level};
+use tracing_subscriber::EnvFilter;
 
 #[tokio::main]
 async fn main() {
+    init_tracing();
+
     // Configure the client with your Discord bot token in the environment.
     let bot_token = "ODgyMDQwOTE1ODgyMDMzMjcy.YS1mnQ.MDIEMq0PSNKOifIZ6Sz4sBQ69vs";
 
@@ -32,35 +40,63 @@ async fn main() {
     }
 }
 
+fn init_tracing() {
+    let append_info = |mut f: EnvFilter, list: &[&str], level: &str| {
+        for l in list {
+            f = f.add_directive(format!("{}={}", l, level).parse().unwrap());
+        }
+        f
+    };
+
+    let list = &[
+        "tokio_util",
+        "h2",
+        "rustls",
+        "serenity",
+        "tungstenite",
+        "async_tungstenite",
+        "hyper",
+        "trust_dns_resolver",
+        "trust_dns_proto",
+        "reqwest",
+        "mio",
+        "want",
+        "kube",
+        "tower",
+    ];
+
+    let filter = EnvFilter::from_default_env();
+    let filter = append_info(filter.add_directive(Level::TRACE.into()), list, "info");
+
+    tracing_subscriber::FmtSubscriber::builder()
+        .with_max_level(Level::TRACE)
+        .with_env_filter(filter)
+        .try_init()
+        .unwrap();
+
+    debug!("tracing initialized");
+}
+
 struct Handler;
 
-#[derive(strum_macros::Display)]
+#[derive(EnumString)]
 pub enum Commands {
+    #[strum(serialize = "start")]
     Start,
+    #[strum(serialize = "shuffle")]
     Shuffle,
 }
 
-#[async_trait]
-impl EventHandler for Handler {
+impl Handler {
     async fn interaction_create(
         &self,
         ctx: Context,
         interaction: Interaction,
     ) -> Result<(), Box<dyn std::error::Error>> {
-        if let Interaction::ApplicationCommand(command) = interaction {
-            // START ROOMS COMMANDS
-            // look for category with certain name to put channels in
-            // If it doesn't exist, tell them to create it
-            // if it exsists, create channels depending on number of users
-
+        error!("Interaction created");
+        if let Interaction::ApplicationCommand(command) = interaction.clone() {
             // SET ROOM MAX
             // decides how many users can be in a room
-
-            // SHUFFLE ROOMS
-            // see if it's the shuffle command
-            // get all users in rooms that are owned by the bot
-            // shuffle them into rooms based on how many users there are
-            // add or remove rooms depending on size max
 
             // STATUS
             // gives info about room max and stuff
@@ -73,12 +109,149 @@ impl EventHandler for Handler {
                 return Ok(());
             };
 
-            match &slash_command.data.name[..] {
-                Commands::Start.into() => {},
-                Commands::Shuffle.into() => {}
+            match Commands::from_str(&slash_command.data.name[..]).unwrap() {
+                Commands::Start => {
+                    warn!("Start command");
+
+                    let categories = ctx.cache.categories().await;
+                    let guild_id = command.guild_id.unwrap();
+                    let guild = guild_id
+                        .to_guild_cached(&ctx)
+                        .await
+                        .ok_or("Cannot get guild")?;
+
+                    // Try to get the category to be used for speed friending
+                    let speed_friend_category = guild
+                        .channels
+                        .iter()
+                        .map(|(_, guild_channel)| guild_channel)
+                        .filter(|guild_channel| {
+                            guild_channel.kind == ChannelType::Category
+                                && guild_channel.name.as_str().to_lowercase() == "speed friending"
+                        })
+                        .collect::<Vec<&GuildChannel>>();
+
+                    // There should only be one category named "Speed Friending"
+
+                    if speed_friend_category.len() == 0 {
+                        command
+                            .create_interaction_response(&ctx.http, |response| {
+                                response
+                                    .kind(InteractionResponseType::ChannelMessageWithSource)
+                                    .interaction_response_data(|message| {
+                                        message
+                                            .content(format!("No speed friending category found"))
+                                    })
+                            })
+                            .await?;
+                        return Ok(());
+                    } else if speed_friend_category.len() > 1 {
+                        command
+                            .create_interaction_response(&ctx.http, |response| {
+                                response
+                                    .kind(InteractionResponseType::ChannelMessageWithSource)
+                                    .interaction_response_data(|message| {
+                                        message.content(format!(
+                                            "Multiple speed friending categories found"
+                                        ))
+                                    })
+                            })
+                            .await?;
+                        return Ok(());
+                    }
+
+                    warn!("TEST");
+
+                    // Make sure there are no channels in the category
+                    let existing_channels = guild
+                        .channels
+                        .iter()
+                        .map(|(_, guild_channel)| guild_channel)
+                        .filter(|guild_channel| match guild_channel.category_id {
+                            Some(category_id) => category_id == speed_friend_category[0].id,
+                            None => false,
+                        })
+                        .collect::<Vec<&GuildChannel>>();
+
+                    if existing_channels.len() > 0 {
+                        // command.create_interaction_response(&ctx.http, |response| {
+                        //     response
+                        //         .kind(InteractionResponseType::ChannelMessageWithSource)
+                        //         .interaction_response_data(|message| {
+                        //             message.content(format!("Channels already exist in speed friending category, please remove them first"))
+                        //         })
+                        // }).await?;
+                        // return Ok(());
+                        
+                        for channel in existing_channels {
+                            warn!("Deleting channel {}", channel.name);
+                            channel.delete(&ctx.http).await?;
+                        }
+                        
+                    }
+
+                    // Create the channels
+                    for i in 0..2 {
+                        let channel_name = format!("{} {}", "Speed Friending", i + 1);
+                        let channel_category = speed_friend_category[0].id;
+                        let channel_type = ChannelType::Voice;
+                        let channel_permissions = Permissions::empty();
+
+                        let channel = guild
+                            .create_channel(&ctx.http, |c| {
+                                c.name(channel_name)
+                                    .kind(channel_type)
+                                    .category(channel_category)
+                            })
+                            .await?;
+                    }
+
+                    // create channels depending on number of users
+
+                    // debug!(
+                    //     "{:?}",
+                    //     channels
+                    //         .iter()
+                    //         .filter(|(_, x)| x.kind == ChannelType::Category)
+                    //         .map(|(_, x)| x.name.as_str())
+                    //         .collect::<Vec<&str>>()
+                    // );
+
+                    // warn!("{:#?}", categories);
+                    // START ROOMS COMMANDS
+                    // look for category with certain name to put channels in
+                    // If it doesn't exist, tell them to create it
+
+                    command
+                        .create_interaction_response(&ctx.http, |response| {
+                            response
+                                .kind(InteractionResponseType::ChannelMessageWithSource)
+                                .interaction_response_data(|message| {
+                                    message.content(format!("Channels created!"))
+                                })
+                        })
+                        .await?;
+                }
+                Commands::Shuffle => {
+                    warn!("Shuffle command");
+                    // SHUFFLE ROOMS
+                    // see if it's the shuffle command
+                    // get all users in rooms that are owned by the bot
+                    // shuffle them into rooms based on how many users there are
+                    // add or remove rooms depending on size max
+                }
             }
         }
         Ok(())
+    }
+}
+
+#[async_trait]
+impl EventHandler for Handler {
+    async fn interaction_create(&self, context: Context, interaction: Interaction) {
+        if let Err(e) = self.interaction_create(context, interaction).await {
+            error!(?e, "Error while processing message");
+        }
     }
 
     async fn ready(&self, ctx: Context, ready: Ready) {
